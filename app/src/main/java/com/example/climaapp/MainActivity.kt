28da2ac7fe309956b99e5dbd79708a784349a621
Matias <barretomatiasgabriel@gmail.com>
2025-05-27 19:31:12 -0300

    probando rama

--- app/src/main/java/com/example/climaapp/MainActivity.kt
+++ app/src/main/java/com/example/climaapp/MainActivity.kt
@@ -3,11 +3,7 @@ package com.example.climaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import com.example.climaapp.data.tema.TemaViewModel
