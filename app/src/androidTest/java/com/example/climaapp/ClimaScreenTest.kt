ea4b29a4c7f456c39d7c32217b86b1321bbb6f90
Matias <barretomatiasgabriel@gmail.com>
2025-06-02 19:47:40 -0300

    add testing

--- app/src/androidTest/java/com/example/climaapp/ClimaScreenTest.kt
+++ app/src/androidTest/java/com/example/climaapp/ClimaScreenTest.kt
@@ -1,21 +1,15 @@
 package com.example.climaapp
 
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.Modifier.Companion.any
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.climaapp.data.api.KtorClient
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+
+@Suppress("MISSING_DEPENDENCY_CLASS_IN_EXPRESSION_TYPE")
 class ClimaScreenTest {
 
     @get:Rule
